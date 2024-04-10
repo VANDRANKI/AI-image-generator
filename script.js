fd63481e6385dad741676f49f7bc6e0219c5ab8e
VANDRANKI <72809214+VANDRANKI@users.noreply.github.com>
2024-04-11 01:21:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,16 @@
 const generateForm = document.querySelector(".generate-form");
 const generateBtn = generateForm.querySelector(".generate-btn");
 const imageGallery = document.querySelector(".image-gallery");
-
 const OPENAI_API_KEY = "sk-...KlwR"; // Your OpenAI API key here
 let isImageGenerating = false;
-
 const updateImageCard = (imgDataArray) => {
   imgDataArray.forEach((imgObject, index) => {
     const imgCard = imageGallery.querySelectorAll(".img-card")[index];
     const imgElement = imgCard.querySelector("img");
     const downloadBtn = imgCard.querySelector(".download-btn");
-
     //Set the image source to the AI-generated image data
     const aiGeneratedImage = `data:image/jpeg;base64,${imgObject.b64_json}`;
     imgElement.src = aiGeneratedImage;
-
     //When the image is loaded, remove the loading class and set download attributes
     imgElement.onload = () => {
       imgCard.classList.remove("loading");
@@ -23,7 +19,6 @@ const updateImageCard = (imgDataArray) => {
     };
   });
 };
-
 const generateAiImages = async (userPrompt, userImgQuantity) => {
   try {
     //Send a request to the OpenAI API to generate images based on user inputs
